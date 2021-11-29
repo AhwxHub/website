10753fd2217445008aa05491c0f9828730e8168a
Prifixy <87855602+Prifixy@users.noreply.github.com>
2021-11-29 16:58:33 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+if (location.href.includes("prifixy.github.io"))
+{
+    location.replace("https://eilix.xyz");
+}
+        
+if (location.protocol == "http:")
+{
+    location.replace("https://eilix.xyz/")
+}
